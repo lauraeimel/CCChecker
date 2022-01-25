a87d7d0e424a006480cea6c8eccf023470270e31
L-E-Imel <93445437+L-E-Imel@users.noreply.github.com>
2022-01-24 22:16:02 -0500

    Codecademy JS project
    Using Luhn algorithm to determine if CC#'s are valid

---
+++ main.js
@@ -0,0 +1,88 @@
+// All valid credit card numbers
+const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8]
+const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9]
+const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6]
+const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5]
+const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6]
+
+// All invalid credit card numbers
+const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5]
+const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3]
+const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4]
+const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5]
+const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4]
+
+// Can be either valid or invalid
+const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4]
+const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9]
+const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3]
+const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]
+const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
+
+// An array of all the arrays above
+const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
+
+
+// Add your functions below:
+let validateCred = array => {
+    let luhnArr = [];
+    for (i = array.length - 2; i >= 0; i -= 2) {
+        let eodigit = array[i];
+        eodigit *= 2;
+        if (eodigit > 9) {
+            eodigit = eodigit -= 9;
+        };
+        luhnArr.push(eodigit);
+    };
+    for (i = array.length - 1; i >= 0; i -= 2) {
+        luhnArr.push(array[i]);
+    };
+    let reduced = luhnArr.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+    });
+    //console.log(reduced);
+    if (reduced % 10 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const findInvalidCards = nestArr => {
+    let invalidCards = nestArr.filter(arr => {
+        return !validateCred(arr);
+    });
+    return invalidCards;
+}
+
+const idInvalidCardCompanies = nestArr => {
+    let firstNum = [];
+    for (let i = 0; i < nestArr.length; i++) {
+        firstNum.push(nestArr[i][0]);
+    };
+    console.log(firstNum);
+    const invalidComp = [];
+    if (firstNum.includes(3)) {
+        invalidComp.push('American Express');
+    };
+    if (firstNum.includes(4)) {
+        invalidComp.push('Visa');
+    };
+    if (firstNum.includes(5)) {
+        invalidComp.push('Mastercard');
+    };
+    if (firstNum.includes(6)) {
+        invalidComp.push('Discover');
+    };
+    if (firstNum.includes(0) || firstNum.includes(1) || firstNum.includes(2) || firstNum.includes(7) || firstNum.includes(8) || firstNum.includes(9)) {
+        invalidComp.push('Company Unknown');
+    };
+    return invalidComp;
+}
+
+
+
+
+
+
+
